000e610fbad92e8e8ed043d3d680657017ffb87f
Nahu_cellone <nahuelcellone@gmail.com>
2022-10-31 19:36:43 -0300

    agregue font-awesome y los iconos a los tasks-items

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[]=[
+  {
+    id: 1,
+    text: "terminar primer modulo de angular",
+    day: 'Agosto 5 a las 12:00',
+    reminder : true, 
+  },
+  {
+    id: 2,
+    text: "terminar segundo modulo de angular",
+    day: 'Agosto 5 a las 17:00',
+    reminder : true, 
+  },
+  {
+    id: 3,
+    text: "terminar tercer modulo de angular",
+    day: 'Agosto 5 a las 21:00',
+    reminder : false, 
+  },
+
+]
